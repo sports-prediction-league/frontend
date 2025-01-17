c32bc7bb66db8c430d95737ad2c651dac81f235b
zarah <devvick230385@gmail.com>
2025-01-17 12:13:03 +0100

    fix: fix profile ranking

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -532,7 +532,7 @@ function App() {
   //     })();
   //   }
   // }, [connected_address]);
-  const [res, set_res] = useState("");
+  // const [res, set_res] = useState("");
   useEffect(() => {
     if (connected_address && profile && leaderboard.length) {
       const find_index = leaderboard.findIndex(
@@ -540,25 +540,25 @@ function App() {
           fd.user?.address?.toLowerCase() ===
             connected_address?.toLowerCase() || fd.user.id === profile?.id
       );
-      const find = leaderboard.find(
-        (fd) =>
-          fd.user?.address?.toLowerCase() ===
-            connected_address?.toLowerCase() || fd.user.id === profile?.id
-      );
-      set_res(
-        find
-          ? JSON.stringify(find)
-          : JSON.stringify({
-              id: Number(leaderboard[0].user?.id ?? 0),
-              idd: profile?.id,
-              is_the_same:
-                leaderboard[0].user?.address?.toLowerCase() ===
-                connected_address?.toLowerCase(),
-              l_arrd: leaderboard[0].user?.address?.toLowerCase(),
-              connected_address,
-              index: find_index,
-            })
-      );
+      // const find = leaderboard.find(
+      //   (fd) =>
+      //     fd.user?.address?.toLowerCase() ===
+      //       connected_address?.toLowerCase() || fd.user.id === profile?.id
+      // );
+      // set_res(
+      //   find
+      //     ? JSON.stringify(find)
+      //     : JSON.stringify({
+      //         id: Number(leaderboard[0].user?.id ?? 0),
+      //         idd: profile?.id,
+      //         is_the_same:
+      //           leaderboard[0].user?.address?.toLowerCase() ===
+      //           connected_address?.toLowerCase(),
+      //         l_arrd: leaderboard[0].user?.address?.toLowerCase(),
+      //         connected_address,
+      //         index: find_index,
+      //       })
+      // );
 
       if (find_index !== -1) {
         dispatch(
@@ -611,7 +611,7 @@ function App() {
 
   return (
     <ThemeProvider>
-      {res}
+      {/* {res} */}
       {splash_active ? null : (
         <RegisterModal
           t_username={profile?.username}
